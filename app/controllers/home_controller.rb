e08b7a55e62a84b555325113d1bda11ccbd29b24
nami634 <cigarbox.nami@gmail.com>
2016-08-02 00:16:28 +0900

    websocket test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
 	def home
-		
+		@channel = SecureRandom.hex(2)
+	end
+	def controller
+		@channel = params[:channel]
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "home#home"
+  get "controller/:channel" => "home#controller"
 end
